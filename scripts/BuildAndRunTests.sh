0a1ef208f81d5eb1dbc390b156d7f9f4ee3d8b18
Joel Stienlet <stienlet@localhost.localdomain>
2020-03-02 13:33:00 +0100

    coverage debug file location

--- scripts/BuildAndRunTests.sh
+++ scripts/BuildAndRunTests.sh
@@ -4,10 +4,10 @@ pwd=`pwd`
 echo "$pwd"
 ls
 sudo BUILD_WITH_COVERAGE=1 python3 setup.py install
-ls -al build
-cd Examples
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+ls -al
 whoami
 sudo chown -R travis:travis build
+ls -al
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+cd Examples
 ./Two_Sinks.py
-
