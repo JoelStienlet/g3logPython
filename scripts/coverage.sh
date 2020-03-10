576b6c726b0b0da58d822ae1b6b93e64973fec31
Joel Stienlet <stienlet@localhost.localdomain>
2020-03-10 12:13:24 +0100

    add g3logBindings to coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,8 +3,9 @@
 pwd
 # /home/travis/build/JoelStienlet/g3logPython
 ls -al
-
+ls build/temp.linux-x86_64-3.6/
 cp build/temp.linux-x86_64-3.6/g3logPython/*.gcda .
+cp build/temp.linux-x86_64-3.6/g3logBindings/*.gcda .
 ls -al
 
 export CODECOV_TOKEN="b5c50253-e6b1-4508-b7f3-800c25231eda"
