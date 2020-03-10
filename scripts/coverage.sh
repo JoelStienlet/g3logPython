881c066377c88ba9783dfd25d46edfb2af6e5688
Joel Stienlet <stienlet@localhost.localdomain>
2020-03-10 12:43:41 +0100

    debug coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
-pwd
+
 # /home/travis/build/JoelStienlet/g3logPython
+echo "=================="
+pwd
 ls -al
+echo "=================="
+echo "build/temp.linux-x86_64-3.6/"
 ls build/temp.linux-x86_64-3.6/
+echo "=================="
+echo "build_bindings/"
+ls build_bindings/
+echo "=================="
+echo "build_bindings/temp.linux-x86_64-3.6/"
+ls build_bindings/temp.linux-x86_64-3.6/
+echo "=================="
 cp build/temp.linux-x86_64-3.6/g3logPython/*.gcda .
-cp build/temp.linux-x86_64-3.6/g3logBindings/*.gcda .
+cp build_bindings/temp.linux-x86_64-3.6/g3logBindings/*.gcda .
+echo "=================="
+echo "check if the .gcda files are in the upper directory:"
 ls -al
+echo "=================="
 
 export CODECOV_TOKEN="b5c50253-e6b1-4508-b7f3-800c25231eda"
 bash <(curl -s https://codecov.io/bash)
