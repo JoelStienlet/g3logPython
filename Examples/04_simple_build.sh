34b941acfe021877a2246690f1c8ae38e656918f
Joel Stienlet <stienlet@localhost.localdomain>
2020-03-10 13:20:54 +0100

    correct compile flags of example 04

--- Examples/04_simple_build.sh
+++ Examples/04_simple_build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-g++ 04_simple_pure_CPP.cpp -o 04_simple -lg3logBindings  -lg3logger
+echo "extra argument: $1"
+g++ 04_simple_pure_CPP.cpp -o 04_simple -lg3logBindings  -lg3logger $1
