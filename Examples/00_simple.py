652fe24f3ea10ed236d607df8a291854c73419ef
Joel Stienlet <stienlet@localhost.localdomain>
2020-03-27 14:08:00 +0100

    add interoperability with python's built-in logger

--- Examples/00_simple.py
+++ Examples/00_simple.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
-import g3logPython as log
+import g3logPython as g3log
+import logging
 
-logger = log.get_ifaceLogWorker(False)
+logger = g3log.get_ifaceLogWorker(False)
 colorTermSink = logger.ClrTermSinks.new_Sink("color term")
+# optional interoperability with pythons' builtin logger:
+builtinLogger = logging.getLogger()
+builtinLogger.addHandler(g3log.g3logHandler())
+builtinLogger.setLevel(logging.DEBUG)
 
-log.debug("hello world! from python")
+g3log.debug("hello world! from python, g3log used directly")
+logging.info("this message is displayed using python's built-in logger")
+
--- g3logPython/__init__.py
+++ g3logPython/__init__.py
@@ -2,6 +2,7 @@
 import sys
 import inspect
 from functools import partial
+import logging
 from _g3logPython import *
 
 def g3log(level, message):
@@ -11,7 +12,36 @@ def g3log(level, message):
     except Exception as e:
         print("ERROR in g3log python: " + str(e), file=sys.stderr )
 
-
+# https://docs.python.org/3.8/library/logging.html
+# https://docs.python.org/3/howto/logging-cookbook.html
+# https://docs.python.org/3.8/library/logging.html#logging.LogRecord
+class g3logHandler(logging.Handler):
+    def __init__(self, *args, **kwargs):
+        super(g3logHandler, self).__init__(*args, **kwargs)
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            receivelog( record.filename, 
+                        record.lineno, 
+                        record.funcName, 
+                        self.pylevel_to_g3level(record.levelno), 
+                        msg)
+        except Exception as e:
+            print("ERROR in g3log python: " + str(e), file=sys.stderr )
+    def pylevel_to_g3level(self, pylevel):
+        if pylevel == logging.DEBUG:
+            return g3DEBUG
+        elif pylevel == logging.INFO:
+            return g3INFO
+        elif pylevel == logging.WARNING:
+            return g3WARNING
+        elif pylevel == logging.ERROR:
+            return g3WARNING
+        elif pylevel == logging.CRITICAL:
+            return g3FATAL
+        else:
+            return g3FATAL
+        
 debug   = partial(g3log, g3DEBUG)
 info    = partial(g3log, g3INFO)
 warning = partial(g3log, g3WARNING)
