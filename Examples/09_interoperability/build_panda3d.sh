eacb1fb47e575b3af815079bb6f0d26d863c3e2b
stienlet <stienlet@localhost.localdomain>
2021-01-09 18:07:35 +0100

    update interoperability example 09

--- Examples/09_interoperability/build_panda3d.sh
+++ Examples/09_interoperability/build_panda3d.sh
@@ -12,6 +12,8 @@ pwd
 git clone https://github.com/panda3d/panda3d.git
 ls
 cd panda3d
+# d39d40b1fc4dee02ad913e033790a5a6a591c659  --> 1.10.8 
+git checkout d39d40b1fc4dee02ad913e033790a5a6a591c659
 python3 makepanda/makepanda.py --threads 3 --everything --installer --no-opencv 
 # --no-egl --no-gles --no-gles2
 
